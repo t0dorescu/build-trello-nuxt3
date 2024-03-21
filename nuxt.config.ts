8a1f9c126f68af14aac35f09066f0fb83d08ee47
t0dorescu <todorescu.consulting@gmail.com>
2024-03-21 22:17:34 +0200

    #

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["~/assets/css/vello.css"],
   modules: ["@nuxt/ui", "@pinia/nuxt"],
   colorMode: {
     preference: "light",
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,43 @@ const boardStore = useBoardStore();
 </script>
 
 <template>
-  <main>
-    <h1>Build a Trello Clone</h1>
-    <pre>{{ boardStore.board }}</pre>
+  <div class="board-wrapper">
+    <main class="board">
+      <UContainer
+        v-for="column in boardStore.board.columns"
+        :key="column.name"
+        class="column"
+      >
+        <h2 class="mb-4">{{ column.name }}</h2>
+        <ul>
+          <li v-for="task in column.tasks" :key="task.id">
+            <UCard class="mb-4">
+              <strong>{{ task.name }}</strong>
+              <p>{{ task.description }}</p>
+            </UCard>
+          </li>
+        </ul>
+      </UContainer>
+    </main>
+  </div>
+
+  <!--
+<main class="board p-5">
+    <h1 class="text-xl mb-3">{{ boardStore.board.name }}</h1>
+    <div
+      v-for="column in boardStore.board.columns"
+      :key="column.name"
+      class="mb-6 px-6"
+    >
+      <h2 class="text-lg">{{ column.name }}</h2>
+      <ul>
+        <li v-for="task in column.tasks" :key="task.id" class="px-6 mb-6">
+          <strong>{{ task.name }}</strong>
+          <p>
+            {{ task.description }}
+          </p>
+        </li>
+      </ul>
+    </div>
   </main>
-</template>
+--></template>
