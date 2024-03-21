9999719df9bbebb47682960c7eae98cd5b2ceab1
t0dorescu <todorescu.consulting@gmail.com>
2024-03-21 21:53:45 +0200

    #

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const boardStore = useBoardStore();
+</script>
+
+<template>
+  <main>
+    <h1>Build a Trello Clone</h1>
+    <pre>{{ boardStore.board }}</pre>
+  </main>
+</template>
